ebf90e4bcac736675670b637089271f8fcee3e4d
alielbialy4 <110687260+alielbialy4@users.noreply.github.com>
2023-12-08 02:56:31 +0200

    edit

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\Auth\AdminLoginRequest;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Contracts\Auth\StatefulGuard;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Routing\Pipeline;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Fortify\Actions\AttemptToAuthenticate;
+use Laravel\Fortify\Actions\CanonicalizeUsername;
+use Laravel\Fortify\Actions\EnsureLoginIsNotThrottled;
+use Laravel\Fortify\Actions\PrepareAuthenticatedSession;
+use Laravel\Fortify\Actions\RedirectIfTwoFactorAuthenticatable;
+use Laravel\Fortify\Contracts\LoginResponse;
+use Laravel\Fortify\Contracts\LoginViewResponse;
+use Laravel\Fortify\Contracts\LogoutResponse;
+use Laravel\Fortify\Features;
+use Laravel\Fortify\Fortify;
+use Laravel\Fortify\Http\Requests\LoginRequest;
+
+class AdminController extends Controller
+{
+    public function index(){
+        return view('admin.auth.signin');
+    }
+    public function store(AdminLoginRequest $request): RedirectResponse
+    {
+       if($request->authenticate()){
+
+           $request->session()->regenerate();
+
+           return redirect()->intended(RouteServiceProvider::Dashboard);
+       }
+       return redirect()->back()->withErrors('يوجد خطأ في البيانات');
+
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('admin')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
---
+++ resources/views/admin/layouts/head.blade.php
@@ -0,0 +1,19 @@
+<!-- Title -->
+<title> Valex -  Premium dashboard ui bootstrap rwd admin html5 template </title>
+<!-- Favicon -->
+<link rel="icon" href="{{asset('assets/admin/img/brand/favicon.png')}}" type="image/x-icon"/>
+<!-- Icons css -->
+<link href="{{asset('assets/admin/css/icons.css')}}" rel="stylesheet">
+<!--  Custom Scroll bar-->
+<link href="{{asset('assets/admin/plugins/mscrollbar/jquery.mCustomScrollbar.css')}}" rel="stylesheet"/>
+<!--  Sidebar css -->
+<link href="{{asset('assets/admin/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
+<!-- Sidemenu css -->
+<link rel="stylesheet" href="{{asset('assets/admin/css-rtl/sidemenu.css')}}">
+@yield('css')
+<!--- Style css -->
+<link href="{{asset('assets/admin/css-rtl/style.css')}}" rel="stylesheet">
+<!--- Dark-mode css -->
+<link href="{{asset('assets/admin/css-rtl/style-dark.css')}}" rel="stylesheet">
+<!---Skinmodes css-->
+<link href="{{asset('assets/admin/css-rtl/skin-modes.css')}}" rel="stylesheet">
